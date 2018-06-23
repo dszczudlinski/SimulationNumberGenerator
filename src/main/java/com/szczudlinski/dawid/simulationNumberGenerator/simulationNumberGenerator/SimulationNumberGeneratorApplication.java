b6ae8529b04615f1bb8eed3cf8eda4e18286d6ab
Dawid Szczudliński <dszczudlinski@bluesoft.net.pl>
2018-06-23 17:28:03 +0200

    Inicjalny projekt

---
+++ src/main/java/com/szczudlinski/dawid/simulationNumberGenerator/simulationNumberGenerator/SimulationNumberGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.szczudlinski.dawid.simulationNumberGenerator.simulationNumberGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationNumberGeneratorApplication.class, args);
+	}
+}
